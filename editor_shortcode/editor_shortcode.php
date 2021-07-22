07c4e3a4df995ac67d5c6186bd154325fec2b183
nikdow <nik@cbdweb.net>
2021-07-22 16:50:12 +1000

    editor_shortcode: enqueue specific CSS;

--- editor_shortcode/editor_shortcode.php
+++ editor_shortcode/editor_shortcode.php
@@ -1,6 +1,6 @@
 <?php
 
-add_action('wp-enqueue_scripts', 'register_editor_shortcode_css');
+add_action('wp-enqueue_style', 'register_editor_shortcode_css');
 add_shortcode('decody_editor', 'decody_editor');
 
 function decody_editor( $atts )
