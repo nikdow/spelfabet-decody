150c112e7af9b23b2bfe510dc5fc71ee73cc89fe
nikdow <nik@cbdweb.net>
2021-07-22 18:19:48 +1000

    can we show things in a textarea?

--- editor_shortcode/editor_shortcode.js
+++ editor_shortcode/editor_shortcode.js
@@ -1,5 +1,4 @@
 function update(){
-    alert("update clicked!");
     const form = document.forms['decody_editor'];
     const textarea = form.elements.editor;
     textarea.value = "<span style='color: red'>Red span</span><span>No style</span>";
