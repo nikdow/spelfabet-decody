1eb1e72f68169b05bee4017e24e54e3a87800d29
nikdow <nik@cbdweb.net>
2021-07-22 16:10:09 +1000

    editor_shortcode: textarea, button, schema options;

--- editor_shortcode.php
+++ editor_shortcode.php
@@ -4,8 +4,21 @@ add_shortcode('decody_editor', 'decody_editor');
 function decody_editor( $atts )
 {
     ob_start();
+    $tags = get_tags( array(
+            'taxonomy' => 'schema',
+            'orderby' => 'name'
+    ))
     ?>
-    <h2>Spelfabet Decody edit area</h2>
+    <textarea name="editor" placeholder="Enter or paste your text here"></textarea>
+    <button type="button" onclick="update()">check it out</button>
+    <select name="schema">
+        <option value="">Select Schema</option>
+        <?php
+        foreach( $tags as $tag ){
+            ?><option value="<?=$tag->name?>"><?=$tag->name?></option><?php
+        }
+        ?>
+    </select>
     <?php
     return ob_get_clean();
 }
