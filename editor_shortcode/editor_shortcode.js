94dbf01f4834da3269a9abdd6a55d400102d6fba
nikdow <nik@cbdweb.net>
2021-07-22 18:21:36 +1000

    Revert "can we show things in a textarea?"
    This reverts commit 150c112e

--- editor_shortcode/editor_shortcode.js
+++ editor_shortcode/editor_shortcode.js
@@ -1,4 +1,5 @@
 function update(){
+    alert("update clicked!");
     const form = document.forms['decody_editor'];
     const textarea = form.elements.editor;
     textarea.value = "<span style='color: red'>Red span</span><span>No style</span>";
