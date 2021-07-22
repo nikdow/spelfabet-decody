afa006aad32c80f22fcdba7d0fb33c04b7c74992
nikdow <nik@cbdweb.net>
2021-07-22 16:36:38 +1000

    editor_shortcode: enqueue specific CSS;

--- editor_shortcode/editor_shortcode.php
+++ editor_shortcode/editor_shortcode.php
@@ -5,12 +5,12 @@ add_shortcode('decody_editor', 'decody_editor');
 
 function decody_editor( $atts )
 {
-    ob_start();
     $tags = get_tags( array(
             'taxonomy' => 'schema',
             'orderby' => 'name'
     ));
     wp_enqueue_style('editor_shortcode');
+    ob_start();
     ?>
         <div id="decody_editor">
             <textarea name="editor" placeholder="Enter or paste your text here"></textarea><br/>
